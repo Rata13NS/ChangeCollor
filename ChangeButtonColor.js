91ed4209076563b1bc98b90db6f28efa9d5ca131
Rata13NS <craciuncalin20@stud.ase.ro>
2024-02-29 16:29:19 +0200

    Complete Project

--- ChangeButtonColor.js
+++ ChangeButtonColor.js
@@ -1,14 +1,25 @@
-let contor = 0
+let contor = 0;
+var button = document.getElementById('changeTheColor');
 
 function changeColor() {
     if (contor === 0) {
-        document.getElementById("chanegeTheColor").bgcolor = "btn btn-warning";
+        button.classList.replace('btn-danger', 'btn-warning');
         ++contor;
     } else if (contor === 1) {
-        document.getElementById("chanegeTheColor").bgcolor = "btn btn-success";
+        button.classList.replace('btn-warning', 'btn-success');
         ++contor;
     } else if (contor === 2) {
-        document.getElementById("chanegeTheColor").bgcolor = "btn btn-danger";
+        button.classList.replace('btn-success', 'btn-danger');
         contor = 0;
+    }
+}
+
+function timeChangeColor() {
+     if (button.classList.contains('btn-danger')) {
+        button.classList.replace('btn-danger', 'btn-warning');
+    } else if (button.classList.contains('btn-warning')) {
+        button.classList.replace('btn-warning', 'btn-success');
+    } else if (button.classList.contains('btn-success')) {
+         button.classList.replace('btn-success', 'btn-danger');
     }
 }
