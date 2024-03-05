1f353eaffdb4e1652b9439b4c3ab26c974597d5a
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-05 16:45:11 +0200

    Update ChangeButtonColor.js

--- ChangeButtonColor.js
+++ ChangeButtonColor.js
@@ -1,25 +1,26 @@
-let contor = 0;
-var button = document.getElementById('changeTheColor');
-
-function changeColor() {
-    if (contor === 0) {
-        button.classList.replace('btn-danger', 'btn-warning');
-        ++contor;
-    } else if (contor === 1) {
-        button.classList.replace('btn-warning', 'btn-success');
-        ++contor;
-    } else if (contor === 2) {
-        button.classList.replace('btn-success', 'btn-danger');
-        contor = 0;
-    }
-}
-
-function timeChangeColor() {
-     if (button.classList.contains('btn-danger')) {
-        button.classList.replace('btn-danger', 'btn-warning');
-    } else if (button.classList.contains('btn-warning')) {
-        button.classList.replace('btn-warning', 'btn-success');
-    } else if (button.classList.contains('btn-success')) {
-         button.classList.replace('btn-success', 'btn-danger');
-    }
-}
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>First project</title>
+  </head>
+  <body onLoad="setInterval(changeColor, 10000)">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h1>This is my first project:</h1>
+                <button id="changeTheColor" type="button" class="btn btn-danger" onClick="changeColor()">
+                    Change color
+                </button>
+            </div>
+        </div>
+    </div>
+    <script 
+      src="ChangeButtonColor.js">
+   </script> 
+  </body>
+</html>
