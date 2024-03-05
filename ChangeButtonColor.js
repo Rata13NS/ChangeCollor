ba297846b915df37cab219756d13c49400394e67
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-05 16:46:09 +0200

    Update2 ChangeButtonColor.js

--- ChangeButtonColor.js
+++ ChangeButtonColor.js
@@ -1,26 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>First project</title>
-  </head>
-  <body onLoad="setInterval(changeColor, 10000)">
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h1>This is my first project:</h1>
-                <button id="changeTheColor" type="button" class="btn btn-danger" onClick="changeColor()">
-                    Change color
-                </button>
-            </div>
-        </div>
-    </div>
-    <script 
-      src="ChangeButtonColor.js">
-   </script> 
-  </body>
-</html>
+let colorIndex = 0;
+var button = document.getElementById('changeTheColor');
+let colors = ['btn-danger', 'btn-warning', 'btn-success'];
+
+function changeColor() {
+        colorIndex= (colorIndex + 1) % colors.length;
+        button.className = "btn custom-btn " + colors[colorIndex];
+}
